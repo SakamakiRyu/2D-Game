ed4d25ddc96e8a2df343e04ba15932c7a1f8a404
SakamakiRyu <potesara031w@gmail.com>
2022-07-06 17:13:38 +0900

    CanonとBulletクラスの作成

--- Assets/CanonController.cs
+++ Assets/CanonController.cs
@@ -1,10 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 /// <summary>
 /// 砲台の操作をするコンポーネント
 /// </summary>
 public class CanonController : MonoBehaviour
 {
+    [SerializeField]
+    private Transform _muzzleTrasnform = default;
+
+    [SerializeField]
+    private BulletController _bullet = default;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+    }
+
+    /// <summary>
+    /// InputSystem用コールバック関数
+    /// </summary>
+    /// <param name="context"></param>
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        // キーが押された時
+        if (context.started)
+        {
+            Fire();
+        }
+    }
+
+    /// <summary>
+    /// 弾を発射する
+    /// </summary>
+    private void Fire()
+    {
+        var bullet = CreateBullet();
+    }
+
+    private BulletController CreateBullet()
+    {
+        return null;
+    }
 }
