fba8786068849c649cf14834b30b774fdc95d5a8
SakamakiRyu <potesara031w@gmail.com>
2022-07-07 23:07:11 +0900

    Canonコンポーネントの作成をした

--- Assets/Scripts/Object/Canon/Canon.cs
+++ Assets/Scripts/Object/Canon/Canon.cs
@@ -1,24 +1,30 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-/// <summary>
-/// 砲台コンポーネント
-/// </summary>
-public class Canon : MonoBehaviour
+namespace UniversWar
 {
-    [SerializeField]
-    private GameObject _bulletPrefab;
-
-    public void OnFire(InputAction.CallbackContext context)
+    /// <summary>
+    /// 砲台コンポーネント
+    /// </summary>
+    public class Canon : MonoBehaviour
     {
-        if (context.started)
+        [SerializeField]
+        private Transform _muzzleTransform;
+
+        [SerializeField]
+        private Bullet _bulletPrefab;
+
+        public void OnFire(InputAction.CallbackContext context)
+        {
+            if (context.started)
+            {
+
+            }
+        }
+
+        public void Fire(Bullet bulletObject, Vector2 velocity)
         {
 
         }
-    }
-
-    public void Fire(GameObject bulletObject,Vector2 velocity)
-    {
-
     }
 }
