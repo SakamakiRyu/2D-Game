9546dbe7693ec9177361a359a8e73e786e03e147
SakamakiRyu <potesara031w@gmail.com>
2022-07-31 23:07:24 +0900

    no message

--- Assets/Scripts/Object/Canon/PlayerCanon.cs
+++ Assets/Scripts/Object/Canon/PlayerCanon.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using DragonPackage;
+using System.Collections;
+using System;
 
 namespace UniversWar
 {
@@ -9,6 +11,17 @@ namespace UniversWar
         [SerializeField]
         private float _bulletSpeed;
 
+        [SerializeField]
+        private int _maxBulletAmount;
+
+        private int CurrentAmount{ get; set; }
+
+        private void Start()
+        {
+            CurrentAmount = _maxBulletAmount;
+        }
+
+        // コールバック
         public void OnFire(InputAction.CallbackContext context)
         {
             if (context.started)
@@ -17,13 +30,26 @@ namespace UniversWar
             }
         }
 
+        // 射撃
         protected override void Fire()
         {
+            if (CurrentAmount <= 0)
+            {
+                ReloadBullet();
+            }
+
             if (ServiceLocator<IBulletManager>.IsValid)
             {
                 var bullet = ServiceLocator<IBulletManager>.Instance.Get(_muzzleTransform.position);
                 bullet.Shoot(Vector2.right, _bulletSpeed);
+                CurrentAmount--;
             }
+        }
+
+        // 弾の装填
+        private void ReloadBullet()
+        {
+            CurrentAmount = _maxBulletAmount;
         }
     }
 }
